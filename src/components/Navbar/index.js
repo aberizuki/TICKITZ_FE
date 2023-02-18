fe45c521f53e19f94796f4329b36dec6a5d36d99
aberizuki <zalaarthurn@gmail.com>
2023-02-18 11:54:22 +0700

    update : responsive header & footer

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -2,24 +2,27 @@ import { Fragment } from "react";
 import { Menu, Transition } from "@headlessui/react";
 import { ChevronDownIcon } from "@heroicons/react/20/solid";
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
 function classNames(...classes) {
   return classes.filter(Boolean).join(" ");
 }
 
 export default function Navbar() {
+  const [isNavOpen, setIsNavOpen] = useState(false);
+
   return (
     <>
       <header className="w-screen h-[100px]">
         <div className="w-full p-2 flex items-center justify-between">
-          <section className="flex items-center justify-evenly ml-[200px]">
+          <section className="flex lg:items-center lg:justify-evenly lg:ml-[80px]">
             <div className="flex">
               <img
                 className="m-10"
                 src={require("src/assets/img/tickitzpurple.png")}
               />
             </div>
-            <div className="flex">
+            <div className="hidden md:hidden lg:flex">
               <div className="mx-5 font-semibold text-[#414141]">Movies</div>
               <div className="mx-5 font-semibold text-[#414141]">Cinemas</div>
               <div className="mx-5 font-semibold text-[#414141]">
@@ -27,8 +30,75 @@ export default function Navbar() {
               </div>
             </div>
           </section>
-          <section className="flex items-center justify-center mr-[200px]">
-            <div className="m-2">
+          <section>
+            <div className="flex items-center justify-between  border-gray-400 py-8">
+              <nav>
+                <section className="MOBILE-MENU flex lg:hidden mr-5">
+                  <div
+                    className="HAMBURGER-ICON space-y-2"
+                    onClick={() => setIsNavOpen((prev) => !prev)}
+                  >
+                    <span className="block h-0.5 w-8 animate-pulse bg-gray-600"></span>
+                    <span className="block h-0.5 w-8 animate-pulse bg-gray-600"></span>
+                    <span className="block h-0.5 w-8 animate-pulse bg-gray-600"></span>
+                  </div>
+
+                  <div className={isNavOpen ? "showMenuNav" : "hideMenuNav"}>
+                    <div
+                      className="absolute top-0 right-0 px-8 py-8"
+                      onClick={() => setIsNavOpen(false)}
+                    >
+                      <svg
+                        className="h-8 w-8 text-gray-600"
+                        viewBox="0 0 24 24"
+                        fill="none"
+                        stroke="currentColor"
+                        strokeWidth="2"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                      >
+                        <line x1="18" y1="6" x2="6" y2="18" />
+                        <line x1="6" y1="6" x2="18" y2="18" />
+                      </svg>
+                    </div>
+                    <ul className="flex flex-col items-center justify-between min-h-[250px]">
+                      <li className=" border-gray-400 my-8 font-semibold">
+                        <a href="/about">Movies</a>
+                      </li>
+                      <li className=" border-gray-400 my-8 font-semibold">
+                        <a href="/portfolio">Cinemas</a>
+                      </li>
+                      <li className=" border-gray-400 my-8 font-semibold">
+                        <a href="/contact">Buy Ticket</a>
+                      </li>
+                    </ul>
+                  </div>
+                </section>
+              </nav>
+              <style>{`
+      .hideMenuNav {
+        display: none;
+      }
+      .showMenuNav {
+        display: block;
+        position: absolute;
+        width: 100%;
+        height: 100vh;
+        top: 0;
+        left: 0;
+        background: white;
+        z-index: 10;
+        display: flex;
+        flex-direction: column;
+        justify-content: space-evenly;
+        align-items: center;
+      }
+    `}</style>
+            </div>
+          </section>
+
+          <section className="hidden md:hidden lg:flex items-center justify-evenly mr-[80px]">
+            <div className="m-2 hidden md:hidden lg:flex">
               <Menu as="div" className="relative inline-block text-left">
                 <div>
                   <Menu.Button className="inline-flex w-full justify-center rounded-md bg-white px-4 py-2 text-sm font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 focus:ring-offset-gray-100">
@@ -118,24 +188,19 @@ export default function Navbar() {
                 </Transition>
               </Menu>
             </div>
-            <div className="m-10">
-              <img className="m-2" src={require("src/assets/img/search.png")} />
+            <div className="m-2 hidden md:hidden lg:flex">
+              <img
+                className="mx-10"
+                src={require("src/assets/img/search.png")}
+              />
             </div>
-            <div className="m-2 flex">
+            <div className="m-2 hidden md:hidden lg:flex">
               <Link to="/user-login">
                 <button
                   type="submit"
-                  className="bg-[#5F2EEA] text-white font-bold py-2 w-[130px] h-[50px] w-full rounded-xl mx-2"
+                  className="bg-[#5F2EEA] text-white font-bold py-2 w-[130px] h-[50px] w-[150px] rounded-xl mx-2"
                 >
                   Sign in
-                </button>
-              </Link>
-              <Link to="/user-regist">
-                <button
-                  type="submit"
-                  className="bg-white text-[#5F2EEA] border border-[#5F2EEA] font-bold py-2 w-[130px] h-[50px] w-full rounded-xl mx-2"
-                >
-                  Sign up
                 </button>
               </Link>
             </div>
